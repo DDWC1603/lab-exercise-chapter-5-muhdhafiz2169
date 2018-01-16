5bf6c8d47003b6fcf17a92cda66e611837d9f07e
muhdhafiz2169 <33885159+muhdhafiz2169@users.noreply.github.com>
2018-01-16 10:46:27 +0800

    Update Exercise-Function-2.cpp

--- Exercise-Function-2.cpp
+++ Exercise-Function-2.cpp
@@ -1,9 +1,21 @@
 //this is an unfinish program
 //finish up this program
+#include <iostream>
+
+using namespace std;
+
+int sum(int x, int y);
+
+int main() {
+	int num1 = 2, num2 = 1;
+	
+	sum(num1, num2);
+}
 
 int sum(int x,int y)
 {
 	int result;
 	result = x+y;
-
+	
+	cout << result;
 }
